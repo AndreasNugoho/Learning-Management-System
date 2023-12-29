21d713a9881ed029104105c9b3b16c835c532247
AndreasNugroho <andreas.nugroho@ti.ukdw.ac.id>
2023-12-29 19:01:42 +0700

    add: custom admin login and dashboard admin

--- routes/web.php
+++ routes/web.php
@@ -37,9 +37,10 @@ require __DIR__.'/auth.php';
 //// Admin group middleware
 Route::middleware(['auth','roles:admin'])->group(function(){
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
 });
 
-
+Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
 
 
 //// Instructor group middleware
