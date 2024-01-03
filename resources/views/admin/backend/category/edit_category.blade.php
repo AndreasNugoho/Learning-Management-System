1aabac8a76285000d25f703b4d54bd50b798caf3
AndreasNugroho <andreas.nugroho@ti.ukdw.ac.id>
2024-01-03 14:56:18 +0700

    fix: Category CRUD

--- resources/views/admin/backend/category/edit_category.blade.php
+++ resources/views/admin/backend/category/edit_category.blade.php
@@ -5,13 +5,13 @@
     <div class="page-content">
         <!--breadcrumb-->
         <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
-            <div class="breadcrumb-title pe-3">Add Category</div>
+            <div class="breadcrumb-title pe-3">Edit Category</div>
             <div class="ps-3">
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb mb-0 p-0">
                         <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
                         </li>
-                        <li class="breadcrumb-item active" aria-current="page">Add Category</li>
+                        <li class="breadcrumb-item active" aria-current="page">Edit Category</li>
                     </ol>
                 </nav>
             </div>
@@ -20,13 +20,17 @@
         <!--end breadcrumb-->
         <div class="card">
             <div class="card-body p-4">
-                <h5 class="mb-4">Add Category</h5>
-                <form id="myForm" action="{{route('store.category')}}" method="post" class="row g-3" enctype="multipart/form-data">
+                <h5 class="mb-4">Edit Category</h5>
+                <form id="myForm" action="{{ route('update.category') }}" method="post" class="row g-3"
+                    enctype="multipart/form-data">
                     @csrf
+
+                    <input type="text" name="id" value="{{ $category->id }}">
+
                     <div class="form-group col-md-6">
                         <label for="input1" class="form-label">Category Name</label>
                         <input type="text" name="category_name" class="form-control" id="input1"
-                            placeholder="First Name">
+                            placeholder="First Name" value="{{ $category->category_name }}">
                     </div>
 
                     <div class="col-md-6">
@@ -38,7 +42,7 @@
                     </div>
 
                     <div class="col-md-6">
-                        <img id="showImage" src="{{ url('upload/no_image.jpg') }}" class="rounded-circle p-1 bg-primary"
+                        <img id="showImage" src="{{ asset($category->image) }}" class="rounded-circle p-1 bg-primary"
                             width="80">
                     </div>
 
@@ -52,42 +56,6 @@
         </div>
 
     </div>
-
-    <script type="text/javascript">
-        $(document).ready(function() {
-            $('#myForm').validate({
-                rules: {
-                    category_name: {
-                        required: true,
-                    },
-                    image: {
-                        required: true,
-                    },
-
-                },
-                messages: {
-                    category_name: {
-                        required: 'Please Enter Category Name',
-                    },
-                    image: {
-                        required: 'Please Select Category Image',
-                    },
-
-                },
-                errorElement: 'span',
-                errorPlacement: function(error, element) {
-                    error.addClass('invalid-feedback');
-                    element.closest('.form-group').append(error);
-                },
-                highlight: function(element, errorClass, validClass) {
-                    $(element).addClass('is-invalid');
-                },
-                unhighlight: function(element, errorClass, validClass) {
-                    $(element).removeClass('is-invalid');
-                },
-            });
-        });
-    </script>
 
     <script type="text/javascript">
         $(document).ready(function() {
