97474c9c4220352eec2362d8434ae85e34ba8a80
AndreasNugroho <andreas.nugroho@ti.ukdw.ac.id>
2024-01-04 01:09:08 +0700

    add: SubCategory CRUD

--- app/Http/Controllers/Backend/CategoryController.php
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -20,25 +20,21 @@ class CategoryController extends Controller
     {
         return view('admin.backend.category.add_category');
     }
-
     public function StoreCategory(Request $request)
     {
         $image = $request->file('image');
         $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
         Image::make($image)->resize(370, 246)->save('upload/category/' . $name_gen);
         $save_url = 'upload/category/' . $name_gen;
-
         Category::insert([
             'category_name' => $request->category_name,
-            'category_slug' => strtolower(str_replace('', '-', $request->category_name)),
+            'category_slug' => strtolower(str_replace(' ', '-', $request->category_name)),
             'image' => $save_url,
         ]);
-
         $notification = array(
-            'message' => 'Category Inserted Successfully!',
+            'message' => 'Category Inserted Successfully',
             'alert-type' => 'success'
         );
-
         return redirect()->route('all.category')->with($notification);
     }
 
@@ -105,11 +101,35 @@ class CategoryController extends Controller
         return redirect()->back()->with($notification);
     }
 
-    public function AllSubCategory(){
+    public function AllSubCategory()
+    {
 
         $subcategory = SubCategory::latest()->get();
-        return view('admin.backend.subcategory.all_subcategory',compact('subcategory'));
-
+        return view('admin.backend.subcategory.all_subcategory', compact('subcategory'));
     }
+
+    public function AddSubCategory()
+    {
+
+        $category = Category::latest()->get();
+        return view('admin.backend.subcategory.add_subcategory', compact('category'));
+    }
+
+    public function StoreSubCategory(Request $request)
+    {
+
+        SubCategory::insert([
+            'category_id' => $request->category_id,
+            'subcategory_name' => $request->subcategory_name,
+            'subcategory_slug' => strtolower(str_replace(' ', '-', $request->subcategory_name)),
+
+        ]);
+
+        $notification = array(
+            'message' => 'SubCategory Inserted Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->route('all.subcategory')->with($notification);
+    } // End Method 
 
 }
