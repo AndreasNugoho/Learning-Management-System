2573d2c916af73073650847f0d4912825c738dc6
AndreasNugroho <andreas.nugroho@ti.ukdw.ac.id>
2024-01-04 13:50:48 +0700

    add: Instructor Register

--- resources/views/frontend/body/footer.blade.php
+++ resources/views/frontend/body/footer.blade.php
@@ -15,7 +15,8 @@
                     <ul class="social-icons social-icons-styled">
                         <li class="mr-1"><a href="#" class="facebook-bg"><i class="la la-facebook"></i></a></li>
                         <li class="mr-1"><a href="#" class="twitter-bg"><i class="la la-twitter"></i></a></li>
-                        <li class="mr-1"><a href="#" class="instagram-bg"><i class="la la-instagram"></i></a></li>
+                        <li class="mr-1"><a href="#" class="instagram-bg"><i class="la la-instagram"></i></a>
+                        </li>
                         <li class="mr-1"><a href="#" class="linkedin-bg"><i class="la la-linkedin"></i></a></li>
                     </ul>
                 </div><!-- end footer-item -->
@@ -27,7 +28,7 @@
                     <ul class="generic-list-item">
                         <li><a href="#">About us</a></li>
                         <li><a href="#">Contact us</a></li>
-                        <li><a href="#">Become a Teacher</a></li>
+                        <li><a href="{{ route('become.instructor') }}">Become a Teacher</a></li>
                         <li><a href="#">Support</a></li>
                         <li><a href="#">FAQs</a></li>
                         <li><a href="#">Blog</a></li>
@@ -54,8 +55,10 @@
                     <span class="section-divider section--divider"></span>
                     <div class="mobile-app">
                         <p class="pb-3 lh-24">Download our mobile app and learn on the go.</p>
-                        <a href="#" class="d-block mb-2 hover-s"><img src="images/appstore.png" alt="App store" class="img-fluid"></a>
-                        <a href="#" class="d-block hover-s"><img src="images/googleplay.png" alt="Google play store" class="img-fluid"></a>
+                        <a href="#" class="d-block mb-2 hover-s"><img src="images/appstore.png" alt="App store"
+                                class="img-fluid"></a>
+                        <a href="#" class="d-block hover-s"><img src="images/googleplay.png"
+                                alt="Google play store" class="img-fluid"></a>
                     </div>
                 </div><!-- end footer-item -->
             </div><!-- end col-lg-3 -->
@@ -66,7 +69,8 @@
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-lg-6">
-                    <p class="copy-desc">&copy; 2021 Aduca. All Rights Reserved. by <a href="https://techydevs.com/">TechyDevs</a></p>
+                    <p class="copy-desc">&copy; 2021 Aduca. All Rights Reserved. by <a
+                            href="https://techydevs.com/">TechyDevs</a></p>
                 </div><!-- end col-lg-6 -->
                 <div class="col-lg-6">
                     <div class="d-flex flex-wrap align-items-center justify-content-end">
