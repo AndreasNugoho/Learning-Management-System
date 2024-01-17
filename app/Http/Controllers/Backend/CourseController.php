346eaec8437d73c045cda85675735336e4df08fc
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2024-01-17 11:49:30 +0700

    add: course lecture setup

--- app/Http/Controllers/Backend/CourseController.php
+++ app/Http/Controllers/Backend/CourseController.php
@@ -255,7 +255,11 @@ class CourseController extends Controller
     return redirect()->back()->with($notification);
   } // End Method 
 
+  public function AddCourseLecture($id)
+  {
 
+    $course = Course::find($id);
 
-
+    return view('instructor.courses.section.add_course_lecture', compact('course'));
+  }
 }
